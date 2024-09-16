1830ef6ba4890a55938d0f27e90fd64a75819615
sptrains <pssreddy01@gmail.com>
2024-09-16 10:53:08 +0530

    Commit

--- PowerShell/CreateListItem.ps1
+++ PowerShell/CreateListItem.ps1
@@ -1,9 +1,13 @@
 # Parameters
 param(
-    [string]$siteUrl = "https://sptrains.sharepoint.com/sites/TestSite",
-    [string]$clientId = "98b129d0-cde8-4649-a981-550a7fae77b0",
-    [string]$clientSecret = "YBDSXmRvznRjZhRoOxp95nm+3ZsONw2tu2M1hHTgvOY=",
-    [string]$listName = "DestinationList",
+    # [string]$siteUrl = "https://sptrains.sharepoint.com/sites/TestSite",
+    # [string]$clientId = "98b129d0-cde8-4649-a981-550a7fae77b0",
+    # [string]$clientSecret = "YBDSXmRvznRjZhRoOxp95nm+3ZsONw2tu2M1hHTgvOY=",
+    # [string]$listName = "DestinationList",
+    [string]$siteUrl = "",
+    [string]$clientId = "",
+    [string]$clientSecret = "",
+    [string]$listName = "",
     [string]$title = "Title3",
     [string]$status = "Started"
 )
