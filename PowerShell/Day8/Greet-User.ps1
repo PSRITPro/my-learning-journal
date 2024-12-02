85675c0e7c08d1fafcaa22f3c3fb6c439bc4e746
sptrains <pssreddy01@gmail.com>
2024-12-03 00:40:31 +0530

    Commit

---
+++ PowerShell/Day8/Greet-User.ps1
@@ -0,0 +1,7 @@
+# Define the function
+function Greet-User {
+    Write-Host "Hello, Guest!"  # Output a greeting message without parameters
+}
+
+# Call the function
+Greet-User  # This will output: "Hello, Guest!"
