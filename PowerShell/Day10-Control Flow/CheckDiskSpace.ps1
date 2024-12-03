850afb4b89580c5dd68c02b20b8e09141c4611aa
sptrains <pssreddy01@gmail.com>
2024-12-03 19:32:40 +0530

    commit

--- PowerShell/Day10-Control Flow/CheckDiskSpace.ps1
+++ PowerShell/Day10-Control Flow/CheckDiskSpace.ps1
@@ -15,6 +15,11 @@ $passwordFile = "Pass.txt"
 $subject = "Disk Space Alert"
 $body = ""
 
+# Read the password from a file
+$txt = Get-Content $passwordFile
+$securePassword = ConvertTo-SecureString $txt -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($from, $securePassword)
+
 # Check the free space and display appropriate message
 if ($freeSpaceGB -lt $threshold) {
     $message = "Warning: Low disk space on C: drive. Free space: $freeSpaceGB GB"
@@ -22,19 +27,19 @@ if ($freeSpaceGB -lt $threshold) {
     Write-Host $message -ForegroundColor Red
     
     # Send an email notification
-    Send-MailMessage -SmtpServer $smtpServer -From $from -To $to -Subject $subject -Body $body -BodyAsHtml
+    Send-MailMessage -From $from -To $to -Subject $subject -Body $body -BodyAsHtml -SmtpServer $smtpServer -Port $smtpPort -UseSsl -Credential $credential -Priority High
 } elseif ($freeSpaceGB -eq $threshold) {
     $message = "Alert: Disk space on C: drive is exactly $threshold GB."
     $body = "Disk space on C: drive is exactly at the threshold. $message"
     Write-Host $message -ForegroundColor Yellow
 
     # Send an email notification
-    Send-MailMessage -SmtpServer $smtpServer -From $from -To $to -Subject $subject -Body $body -BodyAsHtml
+    Send-MailMessage -From $from -To $to -Subject $subject -Body $body -BodyAsHtml -SmtpServer $smtpServer -Port $smtpPort -UseSsl -Credential $credential -Priority High
 } else {
     $message = "Success: Sufficient disk space on C: drive. Free space: $freeSpaceGB GB"
     $body = "Disk space on C: drive is sufficient. $message"
     Write-Host $message -ForegroundColor Green
     
     # Send an email notification
-    Send-MailMessage -SmtpServer $smtpServer -From $from -To $to -Subject $subject -Body $body -BodyAsHtml
+    Send-MailMessage -From $from -To $to -Subject $subject -Body $body -BodyAsHtml -SmtpServer $smtpServer -Port $smtpPort -UseSsl -Credential $credential -Priority High
 }
